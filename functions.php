3c4d250ce9aeccdcca0a96ddf129b9e90216e822
wkwkrnht <wkwkrnht@gmail.com>
2016-09-24 12:28:13 +0900

    fix

--- functions.php
+++ functions.php
@@ -1,2 +1,2 @@
 <?php
-add_action('wp_enqueue_scripts',function(){wp_enqueue_style('child-style',get_template_directory_uri() . '/style.css');}); 
+add_action('wp_enqueue_scripts',function(){wp_enqueue_style('origin-style',get_template_directory_uri() . '/styles.css');wp_enqueue_style('child-style',get_stylesheet_directory_uri() . '/style.css',array('origin-style'));});
